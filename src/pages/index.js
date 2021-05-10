f6e8cac367cb368a03c85438d3736388341b735c
Jarmo Isotalo <jamo@isotalo.fi>
2021-05-10 21:44:28 +0300

    Error handling1

--- src/pages/index.js
+++ src/pages/index.js
@@ -123,6 +123,10 @@ const links = [
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
+
+
+
+
 ]
 
 // markup
